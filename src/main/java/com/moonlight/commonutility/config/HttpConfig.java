9b88c2415e7aac0fc760415c1d79d763411c78db
chandradip-gslab <chandradip.gslab@turtlemint.com>
2024-01-28 12:37:18 +0530

    headers configuration

---
+++ src/main/java/com/moonlight/commonutility/config/HttpConfig.java
@@ -0,0 +1,29 @@
+package com.moonlight.commonutility.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+@Component
+public class HttpConfig {
+
+    public HttpHeaders getDefaultHeaders() {
+        Map<String, String> headersMap = Map.ofEntries(
+//                entry(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                entry("Content-Type", "application/json")
+        );
+        HttpHeaders headers = new HttpHeaders();
+        headersMap.forEach(headers::set);
+        return headers;
+    }
+
+    public HttpHeaders setHeaders(Map<String, String> map) {
+        HttpHeaders headers = new HttpHeaders();
+        if (map!=null) map.forEach(headers::set);
+        return headers;
+    }
+}
